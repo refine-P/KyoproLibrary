b197b466ec90a39acd52bfa30cb86e270b31c5e6
fine <32488002+refine-P@users.noreply.github.com>
2019-06-04 01:58:01 +0900

    [update] dijkstra

--- dijkstra.hpp
+++ dijkstra.hpp
@@ -1,42 +1,43 @@
 struct State {
     int at;
-    int cost; 
-    int prev;
-    State(int at, int cost, int prev) : at(at), cost(cost), prev(prev) {}
+    ll cost; 
+    //int prev;
+    State(int at, ll cost/*, int prev*/) : at(at), cost(cost)/*, prev(prev)*/ {}
     bool operator>(const State& s) const {
-        if (cost != s.cost) return cost > s.cost;
-        if (prev != s.prev) return prev > s.prev; //最短経路を辞書順最小にする(省略可)
-        return at > s.at;
+        //if (cost != s.cost) return cost > s.cost;
+        //if (prev != s.prev) return prev > s.prev; //最短経路を辞書順最小にする(省略可)
+        //return at > s.at;
+        return cost > s.cost;
     }
 };
 
 struct Edge {
     int to;
-    int cost;  
-    Edge(int to, int cost) : to(to), cost(cost) {}  
+    ll cost;  
+    Edge(int to, ll cost) : to(to), cost(cost) {}  
 };
 
-typedef vector<vector<Edge> > AdjList; //隣接リスト
+using Graph = vector<vector<Edge> >; //隣接リスト
 
-const int INF = 1000000000;
-const int NONE = -1;
-
-AdjList graph;
+const ll INF = 1e15;
+//const int NONE = -1;
 
 //sは始点、mincは最短経路のコスト、Prevは最短経路をたどる際の前の頂点
-void dijkstra(int s, vector<int>& minc, vector<int>& Prev){ 
+void dijkstra(int s, const Graph& graph, vector<ll>& minc/*, vector<int>& Prev*/){
+    minc.assign(graph.size(), INF);
+    //Prev.assign(graph.size(), INF);
     priority_queue<State, vector<State>, greater<State> > pq;
-    pq.push(State(s, 0, NONE));
+    pq.emplace(s, 0/*, NONE*/);
     while(!pq.empty()) {
         State cur = pq.top();
         pq.pop();
         if (minc[cur.at] <= cur.cost) continue;
         minc[cur.at] = cur.cost;
-        Prev[cur.at] = cur.prev;
-        for(Edge e : graph[cur.at]) {           
-            int cost = cur.cost + e.cost;
+        //Prev[cur.at] = cur.prev;
+        for(const Edge& e : graph[cur.at]) {           
+            ll cost = cur.cost + e.cost;
             if (minc[e.to] <= cost) continue;
-            pq.push(State(e.to, cost, cur.at));
+            pq.emplace(e.to, cost/*, cur.at*/);
         }
     }
 }
