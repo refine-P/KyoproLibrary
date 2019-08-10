9b8352675e8eff4e42c50bc7584a4a1a4f1466a9
fine <32488002+refine-P@users.noreply.github.com>
2019-08-10 23:19:09 +0900

    [update] bellman_ford

--- bellman_ford.hpp
+++ bellman_ford.hpp
@@ -1,37 +1,35 @@
 struct Edge {
-  int to;
-  int cost;  
-  Edge(int to, int cost) : to(to), cost(cost) {}  
+    int to;
+    ll cost;
+    Edge(int to, ll cost) : to(to), cost(cost) {}
 };
 
-typedef vector<vector<Edge> > AdjList; //隣接リスト
+using Graph = vector<vector<Edge> >; //隣接リスト
 
-const int INF = 1000000000;
-const int NONE = -1;
+const ll INF = 1e15;
+// const int NONE = -1;
 
-AdjList graph;
-vector<int> minc; //最短経路のコスト
-vector<int> Prev; //最短経路をたどる際の前の頂点
-
-//trueなら負の閉路を検出
-bool bellman_ford(int n, int s) { //nは頂点数、sは始点
+// sは始点、mincは最短経路のコスト、Prevは最短経路をたどる際の前の頂点
+// trueなら負の閉路を検出
+bool bellman_ford(int s, const Graph& graph, vector<ll>& minc/*, vector<int> Prev*/) { 
+    int n = graph.size();
     minc.resize(n, INF);
-    Prev.resize(n, NONE);
+    //Prev.resize(n, NONE);
     minc[s] = 0; //始点までの距離は0
     for (int i = 1; i < n; i++) {
         for (int from = 0; from < n; from++) {
-            for (Edge e : graph[from]) {
+            for (const Edge& e : graph[from]) {
                 if (minc[from] != INF && minc[e.to] > minc[from] + e.cost) {
                     minc[e.to] = minc[from] + e.cost;
-                    Prev[e.to] = from;
+                    //Prev[e.to] = from;
                 }
             }
         }
     }
-    
+
     //負の閉路を検出
     for (int from = 0; from < n; from++) {
-        for (Edge e : graph[from]) {
+        for (const Edge& e : graph[from]) {
             if (minc[from] != INF && minc[e.to] > minc[from] + e.cost) {
                 return true;
             }
