dab68e049f5bd6deb50d9e2383831acc4498e0cc
fine <32488002+refine-P@users.noreply.github.com>
2019-10-21 01:41:43 +0900

    [add] suffix array

---
+++ suffix_array.hpp
@@ -0,0 +1,66 @@
+struct SuffixArray {
+    string str;
+    vector<int> sa;
+
+    SuffixArray() {}
+    SuffixArray(const string& s) : str(s) {
+        constructSA();
+    }
+
+    // O(N (log N)^2)
+    void constructSA() {
+        if (str.empty()) return;
+
+        int n = str.size();
+
+        sa.resize(n + 1);
+        vector<int> rank(n + 1);
+        for (int i = 0; i <= n; i++) {
+            sa[i] = i;
+            rank[i] = (i < n ? str[i] : -1);
+        }
+
+        int len;
+        auto compare_sa = [&](const int i, const int j) {
+            if (rank[i] != rank[j]) return rank[i] < rank[j];
+            else {
+                int ri = (i + len <= n ? rank[i + len] : -1);
+                int rj = (j + len <= n ? rank[j + len] : -1);
+                return ri < rj;
+            }
+        };
+
+        vector<int> tmp(n + 1);
+        for (len = 1; len <= n; len <<= 1) {
+            sort(sa.begin(), sa.end(), compare_sa);
+
+            tmp[sa[0]] = 0;
+            for (int i = 1; i <= n; i++) {
+                tmp[sa[i]] = tmp[sa[i - 1]] + (compare_sa(sa[i - 1], sa[i]) ? 1 : 0);
+            }
+            for (int i = 0; i <= n; i++) {
+                rank[i] = tmp[i];
+            }
+        }
+    }
+
+    int operator[](int idx) const {
+        return sa[idx];
+    }
+
+    // 文字列s以上となる最小の接尾辞について、SAのindexを返す（無いならsa.size()を返す）
+    int lower_bound(const string& s) {
+        int ng = -1, ok = sa.size();
+        while (ok - ng > 1) {
+            int mid = (ok + ng) >> 1;
+            (str.compare(sa[mid], s.size(), s) < 0 ? ng : ok) = mid; 
+        }
+        return ok;
+    }
+
+    // 文字列sが文字列strに含まれるか？
+    bool contain(const string& s) {
+        int idx = lower_bound(s);
+        return idx < sa.size() && str.compare(sa[idx], s.size(), s) == 0;
+    }
+};
