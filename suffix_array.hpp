eaf32d39dfd6bc9a6ab082c191f7494e0d870474
fine <32488002+refine-P@users.noreply.github.com>
2019-10-30 23:13:34 +0900

    [update] Suffix Array

--- suffix_array.hpp
+++ suffix_array.hpp
@@ -1,6 +1,7 @@
 struct SuffixArray {
     string str;
     vector<int> sa;
+    vector<int> rank; // saの逆関数に相当
 
     SuffixArray() {}
     SuffixArray(const string& s) : str(s) {
@@ -14,7 +15,7 @@ struct SuffixArray {
         int n = str.size();
 
         sa.resize(n + 1);
-        vector<int> rank(n + 1);
+        rank.resize(n + 1);
         for (int i = 0; i <= n; i++) {
             sa[i] = i;
             rank[i] = (i < n ? str[i] : -1);
@@ -36,11 +37,9 @@ struct SuffixArray {
 
             tmp[sa[0]] = 0;
             for (int i = 1; i <= n; i++) {
-                tmp[sa[i]] = tmp[sa[i - 1]] + (compare_sa(sa[i - 1], sa[i]) ? 1 : 0);
+                tmp[sa[i]] = tmp[sa[i - 1]] + compare_sa(sa[i - 1], sa[i]);
             }
-            for (int i = 0; i <= n; i++) {
-                rank[i] = tmp[i];
-            }
+            rank.swap(tmp);
         }
     }
 
@@ -49,23 +48,23 @@ struct SuffixArray {
     }
 
     // 文字列s以上となる最小の接尾辞について、SAのindexを返す（無いならsa.size()を返す）
-    int lower_bound(const string& s) {
+    int lower_bound(const string& s) const {
         int ng = -1, ok = sa.size();
         while (ok - ng > 1) {
             int mid = (ok + ng) >> 1;
-            (str.compare(sa[mid], s.size(), s) < 0 ? ng : ok) = mid; 
+            (str.compare(sa[mid], s.size(), s) < 0 ? ng : ok) = mid;
         }
         return ok;
     }
 
     // 文字列sが文字列strに含まれるならSAのindexを、無いなら-1を返す
-    int find(const string& s) {
+    int find(const string& s) const {
         int idx = lower_bound(s);
         return ((idx < sa.size() && str.compare(sa[idx], s.size(), s) == 0) ? idx : -1);
     }
 
     // 文字列sが文字列strに含まれるか？
-    bool contain(const string& s) {
+    bool contain(const string& s) const {
         return find(s) != -1;
     }
 };
