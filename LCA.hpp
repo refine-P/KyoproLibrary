a3a748cbd6d7d88891ac97ee8247df6d07c2ce6a
fine <32488002+refine-P@users.noreply.github.com>
2019-11-10 00:56:39 +0900

    [update] LCA

--- LCA.hpp
+++ LCA.hpp
@@ -1,43 +1,49 @@
-const int MAX_V = 100000;
-const int MAX_LOG_V = 17;
+struct LCA {
+    using Graph = vector< vector<int> >;
 
-vector<int> G[MAX_V];
-int root = 0;
+    const Graph& g;
+    const int LOGV;
+    const int root;
 
-int parent[MAX_LOG_V][MAX_V];
-int depth[MAX_V];
+    vector< vector<int> > parent;
+    vector<int> depth;
 
-void dfs(int v, int p, int d) {
-	parent[0][v] = p;
-	depth[v] = d;
-	for (int i = 0; i < G[v].size(); i++) {
-		if (G[v][i] != p) dfs(G[v][i], v, d + 1);
-	}
-}
+    LCA(const Graph& g, int root = 0) : g(g), LOGV(32 - __builtin_clz(g.size())), root(root) {
+        int V = g.size();
+        parent.resize(LOGV, vector<int>(V));
+        depth.resize(V);
+        
+        dfs(root, -1, 0);
+        for (int k = 0; k + 1 < LOGV; k++) {
+            for (int v = 0; v < V; v++) {
+                if (parent[k][v] < 0) parent[k + 1][v] = -1;
+                else parent[k + 1][v] = parent[k][parent[k][v]];
+            }
+        }
+    }
 
-void init(int V) {
-	dfs(root, -1, 0);
-	for (int k = 0; k + 1 < MAX_LOG_V; k++) {
-		for (int v = 0; v < V; v++) {
-			if (parent[k][v] < 0) parent[k + 1][v] = -1;
-			else parent[k + 1][v] = parent[k][parent[k][v]];
-		}
-	}
-}
+    void dfs(int v, int p, int d) {
+        parent[0][v] = p;
+        depth[v] = d;
+        for (int i : g[v]) {
+            if (i != p) dfs(i, v, d + 1);
+        }
+    }
 
-int lca(int u, int v) {
-	if (depth[u] > depth[v]) swap(u, v);
-	for (int k = 0; k < MAX_LOG_V; k++) {
-		if ((depth[v] - depth[u]) >> k & 1) {
-			v = parent[k][v];
-		}
-	}
-	if (u == v) return u;
-	for (int k = MAX_LOG_V - 1; k >= 0; k--) {
-		if (parent[k][u] != parent[k][v]) {
-			u = parent[k][u];
-			v = parent[k][v];
-		}
-	}
-	return parent[0][u];
-}
+    int operator()(int u, int v) const {
+        if (depth[u] > depth[v]) swap(u, v);
+        for (int k = 0; k < LOGV; k++) {
+            if ((depth[v] - depth[u]) >> k & 1) {
+                v = parent[k][v];
+            }
+        }
+        if (u == v) return u;
+        for (int k = LOGV - 1; k >= 0; k--) {
+            if (parent[k][u] != parent[k][v]) {
+                u = parent[k][u];
+                v = parent[k][v];
+            }
+        }
+        return parent[0][u];
+    }
+};
