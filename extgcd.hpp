a59b08b154c1108cff0b5682d96e5bfb3906abcf
fine <32488002+refine-P@users.noreply.github.com>
2019-11-14 01:36:24 +0900

    [update] extgcd

--- extgcd.hpp
+++ extgcd.hpp
@@ -1,5 +1,8 @@
-int extgcd(int a, int b, int& x, int& y) {
-    int d = a;
+// a * x + b * y == gcd(a, b) なるx, yを計算
+// 返り値はgcd(a, b)
+template<typename T>
+T extgcd(T a, T b, T& x, T& y) {
+    T d = a;
     if (b != 0) {
         d = extgcd(b, a % b, y, x);
         y -= (a / b) * x;
