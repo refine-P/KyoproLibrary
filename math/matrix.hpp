617078734e847c0fc00d76dd16bc3c4512380c00
fine <32488002+refine-P@users.noreply.github.com>
2020-05-12 02:50:31 +0900

    [update] matrix mul

--- math/matrix.hpp
+++ math/matrix.hpp
@@ -1,27 +1,27 @@
 using vec = vector<ll>;
 using mat = vector<vec>;
 
-mat mul(mat& A, mat& B) {
-	mat C(A.size(), vec(B[0].size(), 0));
-	for (int i = 0; i < A.size(); i++) {
-		for (int k = 0; k < B.size(); k++) {
-			for (int j = 0; j < B[0].size(); j++) {
-				C[i][j] += A[i][k] * B[k][j]; 
-			}
-		}
-	}
-	return C;
+mat mul(const mat& A, const mat& B) {
+    mat C(A.size(), vec(B[0].size(), 0));
+    for (int i = 0; i < A.size(); i++) {
+        for (int k = 0; k < B.size(); k++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
 }
 
 mat pow(mat A, ll n) {
-	mat B(A.size(), vec(A.size(), 0));
-	for (int i = 0; i < A.size(); i++) {
-		B[i][i] = 1;
-	}
-	while (n > 0) {
-		if (n & 1) B = mul(B, A);
-		A = mul(A, A);
-		n >>= 1;
-	}
-	return B;
+    mat B(A.size(), vec(A.size(), 0));
+    for (int i = 0; i < A.size(); i++) {
+        B[i][i] = 1;
+    }
+    while (n > 0) {
+        if (n & 1) B = mul(B, A);
+        A = mul(A, A);
+        n >>= 1;
+    }
+    return B;
 }
