269f6d5f066574ab4c4412339c1f5aa7c09b0a74
fine <32488002+refine-P@users.noreply.github.com>
2021-07-02 01:46:39 +0900

    [update] warshall_floyd

--- shortest_path/warshall_floyd.hpp
+++ shortest_path/warshall_floyd.hpp
@@ -1,14 +1,13 @@
-typedef vector<vector<int> > Matrix; //隣接行列
+using Matrix = vector< vector<int> >; //隣接行列
 
 const int INF = 1000000000;
 
-Matrix dist;
-
-//trueなら負の閉路を検出
-bool warshall_floyd(int n) { //nは頂点数
-    for (int k = 0; k < n; k++) { //経由点
-        for (int i = 0; i < n; i++) { //始点
-            for (int j = 0; j < n; j++) { //終点
+// trueなら負の閉路を検出
+bool warshall_floyd(Matrix& dist) {
+    int n = dist.size();
+    for (int k = 0; k < n; k++) {  // 経由点
+        for (int i = 0; i < n; i++) {  // 始点
+            for (int j = 0; j < n; j++) {  // 終点
                 if (dist[i][k] != INF && dist[k][j] != INF) {
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
@@ -16,7 +15,7 @@ bool warshall_floyd(int n) { //nは頂点数
         }
     }
     
-    //負の閉路を検出
+    // 負の閉路を検出
     for (int i = 0; i < n; i++) {
         if (dist[i][i] < 0) return true;
     }
